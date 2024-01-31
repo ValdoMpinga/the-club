1b6fc99f6736eb89a428751334b0a8c0b5e544df
Valdo Mpinga <valdompinga2@gmail.com>
2024-01-31 19:33:41 +0000

    connection with sockets base done

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // Use the cors middleware
+app.use(express.static(__dirname + '/..'));
+
+const server = http.createServer(app);
+
+let io; // Declare io variable here
+
+app.get('/', (req, res) =>
+{
+    res.sendFile(__dirname + '/../index.html');
+});
+
+server.listen(3000, () =>
+{
+    console.log('Listening on port 3000');
+
+    // Initialize Socket.IO server after the HTTP server starts listening for connections
+    io = socketIo(server);
+
+    let state = {}; // This will hold the state of the scene
+
+    io.on('connection', (socket) =>
+    {
+        console.log('New client connected');
+
+        // Send the current state to the newly connected client
+        socket.emit('state', state);
+
+        // Listen for 'update' events from the client and update the state
+        socket.on('update', (newState) =>
+        {
+            state = newState;
+            // Broadcast the updated state to all other clients
+            socket.broadcast.emit('state', state);
+        });
+    });
+});
