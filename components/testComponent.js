2849680c07bd44c8c143de66346b96ef01989153
Valdo Mpinga <valdompinga2@gmail.com>
2024-01-31 13:30:17 +0000

    test component setup done

---
+++ components/testComponent.js
@@ -0,0 +1,25 @@
+AFRAME.registerComponent('test-component', {
+    init: function ()
+    {
+        var el = this.el; // Reference to the entity to which this component is attached
+
+        // Change color on click
+        el.addEventListener('click', function ()
+        {
+            console.log("Clicked!");
+            el.setAttribute('color', getRandomColor());
+        });
+
+        // Function to generate a random color
+        function getRandomColor()
+        {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++)
+            {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+    }
+});
