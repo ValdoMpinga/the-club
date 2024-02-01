c59d9bdffd5093706566f549e0b27be19f6827d0
Valdo Mpinga <valdompinga2@gmail.com>
2024-02-01 16:15:50 +0000

    sphere creating broadcasting working on create

--- server/server.js
+++ server/server.js
@@ -8,8 +8,9 @@ app.use(cors()); // Use the cors middleware
 app.use(express.static(__dirname + '/..'));
 
 const server = http.createServer(app);
+const io = socketIo(server);
 
-let io; // Declare io variable here
+let spheres = []; // Array to store sphere positions
 
 app.get('/', (req, res) =>
 {
@@ -19,47 +20,30 @@ app.get('/', (req, res) =>
 server.listen(3000, () =>
 {
     console.log('Listening on port 3000');
+});
 
-    // Initialize Socket.IO server after the HTTP server starts listening for connections
-    io = socketIo(server);
+// Inside the 'connection' event listener
+io.on('connection', (socket) =>
+{
+    console.log('New client connected');
 
-    let state = []; // This will hold the state of the scene
+    // Send the current state (spheres array) to the newly connected client
+    socket.emit('state', spheres);
 
-    // Inside the 'connection' event listener
-    // Inside the 'connection' event listener
-    io.on('connection', (socket) =>
+    // Emit event to all clients when a new user joins
+    io.emit('newUser');
+
+    // Listen for disconnection events
+    socket.on('disconnect', () =>
     {
-        console.log('New client connected');
-
-        // Send the current state to the newly connected client
-        socket.emit('state', state);
-
-        socket.emit('newUser');
-
-
-        // Generate random coordinates for the new sphere
-        const randomX = Math.random() * 10 - 5; // Range: -5 to 5
-
-        // Create a new sphere entity object
-        const newSphere = {
-            type: 'sphere',
-            position: { x: randomX}
-        };
-
-        // Add the new sphere to the state
-        state.push(newSphere);
-
-        // Broadcast the updated state to all clients
-        io.emit('state', state);
-
-        // Listen for 'addEntity' events from the client and add the entity to the state
-        socket.on('addEntity', (entity) =>
-        {
-            state.push(entity);
-            // Broadcast the updated state to all other clients
-            socket.broadcast.emit('state', state);
-        });
+        console.log('Client disconnected');
     });
 
-
+    // Listen for 'addEntity' events from the client and add the entity to the spheres array
+    socket.on('addEntity', (entity) =>
+    {
+        spheres.push(entity);
+        // Broadcast the updated spheres array to all clients
+        io.emit('state', spheres);
+    });
 });
